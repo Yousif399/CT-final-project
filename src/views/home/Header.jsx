f7bf280062d41ef1d7eef9f2f079fc20f3a0ffd6
Jposeph99 <joseph.ahmed65@yahoo.com>
2023-12-04 19:20:23 -0500

    cart fixed

--- src/views/home/Header.jsx
+++ src/views/home/Header.jsx
@@ -90,10 +90,10 @@ const Header = () => {
         <>
             <header>
 
-                
+
                 <Navbar className='navbar m-b-200' expand='lg'  >
                     <Container fluid  >
-                    <Nav.Link id='app-logo' className="navbar-brand " href="/mine"><img src='src/views/img/Watch IT-1.png' height='120px' width='120px' /></Nav.Link>
+                        <Nav.Link id='app-logo' className="navbar-brand " href="/mine"><img src='src/views/img/Watch IT-1.png' height='120px' width='120px' /></Nav.Link>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="me-auto">
@@ -167,8 +167,8 @@ const Header = () => {
                 </div>
 
 
+                <Footer />
             </header >
-
 
 
         </>
